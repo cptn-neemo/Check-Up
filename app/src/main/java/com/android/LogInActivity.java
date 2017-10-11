c77b60423592c675cb2f57e22f225de7a4aeafb8
cptn-neemo <maxhuddleston@gmail.com>
2017-10-10 22:24:16 -0500

    Added sign in activity and updated gradle

---
+++ app/src/main/java/com/android/LogInActivity.java
@@ -0,0 +1,15 @@
+package com.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.adefault.checkup.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
