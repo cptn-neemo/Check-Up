82f87ca8aa225c760f25e6b8c60ccf16e65726b0
cptn-neemo <maxhuddleston@gmail.com>
2017-10-17 18:18:55 -0500

    Added Firebase Authentication to Check Up

--- app/src/main/java/com/android/LogInActivity.java
+++ app/src/main/java/com/android/LogInActivity.java
@@ -1,13 +1,24 @@
 package com.android;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.adefault.checkup.MainActivity;
 import com.example.adefault.checkup.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.w3c.dom.Text;
 
@@ -18,8 +29,9 @@ public class LogInActivity extends AppCompatActivity {
     private Button mLogInButton;
     private TextView mSignUpText;
 
+    private FirebaseAuth mFireBaseAuth;
+    private FirebaseAuth.AuthStateListener mFireBaseAuthListener;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
@@ -29,17 +41,71 @@ public class LogInActivity extends AppCompatActivity {
         mLogInButton = (Button) findViewById(R.id.LI_login_button);
         mSignUpText = (TextView) findViewById(R.id.LI_signup_text);
 
+        mFireBaseAuth = FirebaseAuth.getInstance();
+
+        mFireBaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user  = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    Intent goToMain = new Intent(LogInActivity.this, MainActivity.class);
+                    startActivity(goToMain);
+                }
+
+            }
+        };
+
+        mSignUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToSignUp = new Intent(LogInActivity.this, SignUpActivity.class);
+                startActivity(goToSignUp);
+            }
+        });
+
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mEmailField.getText().toString();
+                String password = mPasswordField.getText().toString();
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(LogInActivity.this, "Please enter all the required fields.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    signIn(username, password);
+                }
+            }
+        });
+
+    }
+
+    private void signIn(String email, String password) {
+       mFireBaseAuth.signInWithEmailAndPassword(email, password)
+               .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Task: ", "successful");
+                       Intent goToMain = new Intent(LogInActivity.this, MainActivity.class);
+                       startActivity(goToMain);
+
+                       if (!task.isComplete()) {
+                           Log.d("Task", "Not successful");
+                       }
+                   }
+               });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        mFireBaseAuth.addAuthStateListener(mFireBaseAuthListener);
+    }
 
-        mSignUpText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO: Add intent to sign up activity
-            }
-        });
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mFireBaseAuth.removeAuthStateListener(mFireBaseAuthListener);
     }
 }
--- app/src/main/java/com/android/SignUpActivity.java
+++ app/src/main/java/com/android/SignUpActivity.java
@@ -1,13 +1,22 @@
 package com.android;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.adefault.checkup.MainActivity;
 import com.example.adefault.checkup.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -15,6 +24,8 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText mPasswordField;
     private EditText mCheckPasswordField;
     private Button mSignUpButton;
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +37,46 @@ public class SignUpActivity extends AppCompatActivity {
         mCheckPasswordField = (EditText) findViewById(R.id.SU_password_check);
         mSignUpButton = (Button) findViewById(R.id.SU_signup_button);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = mEmailField.getText().toString();
-                String password = mPasswordField.getText().toString();
-                String password_check = mCheckPasswordField.getText().toString();
-
-                if (password.equals(password_check)) {
-                    //TODO: Add FirebaseAuth
+                if (TextUtils.isEmpty(mEmailField.getText()) || TextUtils.isEmpty(mPasswordField.getText())
+                        || TextUtils.isEmpty(mCheckPasswordField.getText())) {
+                    Toast.makeText(SignUpActivity.this, "Please enter all the required fields.", Toast.LENGTH_LONG);
                 }
                 else {
-                    Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_LONG).show();
+                    String email = mEmailField.getText().toString();
+                    String password = mPasswordField.getText().toString();
+                    String password_check = mCheckPasswordField.getText().toString();
+
+                    if (password.equals(password_check)) {
+                        createAccount(email, password);
+                    }
+                    else {
+                        Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
+    }
+
+    public void createAccount(String email, String password) {
+        mFirebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("createUserEmailPassword", "user created");
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Problem with creating user",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                        Intent goToMain = new Intent(SignUpActivity.this, MainActivity.class);
+                        startActivity(goToMain);
+                    }
+                });
     }
 }
